5807e39fd0f6ce8bc6f05e4be70585365a013d1c
Ruchir <ruchir@pdx.edu>
2020-08-22 22:05:59 -0700

    add the onClick functionality

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 import React, { Component } from "react";
 import Players from "./components/Players";
 
+const Squad = [
+  "https://images.daznservices.com/di/library/GOAL/e2/82/axel-tuanzebe-man-utd-2019-20_15l7nsm715acy14rlprwldrh2v.jpg",
+  "https://d3nfwcxd527z59.cloudfront.net/content/uploads/2020/03/06081624/Odion-Ighalo-celebrates-goal-Manchester-United-Derby",
+  "https://i2-prod.manchestereveningnews.co.uk/incoming/article17602478.ece/ALTERNATES/s615/0_GettyImages-1198855234.jpg",
+  "https://cdn.vox-cdn.com/thumbor/aL5AhhT_j-z35NgvXdcMvC_QUGw=/0x0:2400x3000/1200x800/filters:focal(1438x317:1822x701)/cdn.vox-cdn.com/uploads/chorus_image/image/67024646/1254242454.jpg.0.jpg",
+  "https://i0.wp.com/metro.co.uk/wp-content/uploads/2020/03/GettyImages-1211338990.jpg",
+  "https://en.as.com/en/imagenes/2020/06/16/football/1592327308_835474_noticia_normal.jpg",
+  "https://d2x51gyc4ptf2q.cloudfront.net/content/uploads/2020/05/18153514/Jesse-Lingard-Man-Utd-F365.jpg",
+];
+
 export class App extends Component {
-  state = {
-    players: [
-      {
-        id: 1,
-        tuanzebe:
-          "https://images.daznservices.com/di/library/GOAL/e2/82/axel-tuanzebe-man-utd-2019-20_15l7nsm715acy14rlprwldrh2v.jpg",
-      },
-      {
-        id: 2,
-        ighalo:
-          "https://d3nfwcxd527z59.cloudfront.net/content/uploads/2020/03/06081624/Odion-Ighalo-celebrates-goal-Manchester-United-Derby.jpg",
-      },
-      {
-        id: 3,
-        martial:
-          "https://i2-prod.manchestereveningnews.co.uk/incoming/article17602478.ece/ALTERNATES/s615/0_GettyImages-1198855234.jpg",
-      },
-      {
-        id: 4,
-        greenwood:
-          "https://cdn.vox-cdn.com/thumbor/aL5AhhT_j-z35NgvXdcMvC_QUGw=/0x0:2400x3000/1200x800/filters:focal(1438x317:1822x701)/cdn.vox-cdn.com/uploads/chorus_image/image/67024646/1254242454.jpg.0.jpg",
-      },
-      {
-        id: 5,
-        james:
-          "https://i0.wp.com/metro.co.uk/wp-content/uploads/2020/03/GettyImages-1211338990.jpg",
-      },
-      {
-        id: 6,
-        rashford:
-          "https://en.as.com/en/imagenes/2020/06/16/football/1592327308_835474_noticia_normal.jpg",
-      },
-      {
-        id: 7,
-        lingard:
-          "https://d2x51gyc4ptf2q.cloudfront.net/content/uploads/2020/05/18153514/Jesse-Lingard-Man-Utd-F365.jpg",
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = { currentImg: 0 };
+  }
+
+  nextGP() {
+    let current = this.state.currentImg;
+    let next = ++current % Squad.length;
+    this.setState({ currentImg: next });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.nextImg, 5000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return <Players players={this.state.players} />;
+    let img = Squad[this.state.currentImg];
+    return <Players img={img} />;
   }
 }
 
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Team from "./components/Team";
 
 export class Players extends Component {
   render() {
-    return this.props.players.map((player) => (
-      <Team players={this.props.players} />
-    ));
+    let src = this.props.img;
+    return (
+      <div>
+        <h1>Man utd players</h1>
+        <img src={src} />
+      </div>
+    );
   }
 }
-
-Players.propTypes = {
-  player: PropTypes.array.isRequired,
-};
 
 export default Players;
